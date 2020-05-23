d7350f9facfc9b6115c665b91e6cfa71b4445217
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2020-05-23 16:58:42 -0300

    Arquivos da Aula 2

---
+++ javascript/pedeNumero.js
@@ -0,0 +1,10 @@
+function pegaFloat(mensagem)
+{
+    return Number(prompt(mensagem).replace(',', '.'))
+}
+
+function pegaInt(mensagem)
+{
+    return parseInt(prompt(mensagem).replace(',', '.'))
+}
+
